1bb588735e2f96098f75f08563a82bb0c4cfb436
alexbaza <alexdevmega@proton.me>
2026-04-20 16:33:22 -0600

    Integrate SensorModule for IMU data reading
    Initialize sensor and read IMU data in loop.

--- src/main.ino
+++ src/main.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "SensorModule.h"
 
+SensorModule sensor;
+
+void setup() {
+  Serial.begin(115200);
+  sensor.init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  IMUData data = sensor.read();
 
+  Serial.print("AX: ");
+  Serial.println(data.ax);
+  Serial.print("AY: ");
+  Serial.println(data.ay);
+  Serial.print("AZ: ");
+  Serial.println(data.az);
+  Serial.print("Temperature: ");
+  Serial.println(data.temp);
+
+  delay(100);
 }
