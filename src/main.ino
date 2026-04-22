de8c97c41801a7cd25151ef795663fda3a9c6366
alexbaza <alexdevmega@proton.me>
2026-04-22 14:18:31 -0600

    Refactor display handling to use OLED module
    Removed Adafruit SSD1306 display and replaced with OLED initialization and usage.

--- src/main.ino
+++ src/main.ino
@@ -1,31 +1,19 @@
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Fonts/Org_01.h>
+#include <avr/pgmspace.h>
 
 #include "SensorModule.h"
 #include "ScreenModule.h"
+#include "OLED.h"
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-
-Adafruit_SSD1306 displayMovementDebug(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 SensorModule sensor;
 
 void setup() {
   Serial.begin(115200);
 
-  // Init screen
-  if (!displayMovementDebug.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306-1 allocation failed"));
-    for (;;); // Crash
-  }
-
-  displayMovementDebug.clearDisplay();
-  displayMovementDebug.setTextSize(1.0);
-  displayMovementDebug.setTextColor(SSD1306_WHITE);
-  displayMovementDebug.setFont(&Org_01);
+  oled_init();
 
   sensor.init();
 }
@@ -33,11 +21,7 @@ void setup() {
 void loop() {
   IMUData data = sensor.read();
 
-  displayMovementDebug.clearDisplay();
-  displayMovementDebug.setCursor(26, 6);
-  displayMovementDebug.print(F("Movement Info"));
-  showDebugInfo(displayMovementDebug, data);
-  displayMovementDebug.display();
+  showDebugInfo(data);
 
   delay(200);
 }
